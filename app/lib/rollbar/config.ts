78c91b4f9794227ac9c4a1a83d0345fcbf924a48
Martin Pernica <martinpernica@outlook.com>
2026-01-29 23:19:22 +0100

    feat: add versioning support for Rollbar client configuration and update example environment file

--- app/lib/rollbar/config.ts
+++ app/lib/rollbar/config.ts
@@ -7,4 +7,13 @@ const baseConfig = {
 export const clientConfig = {
   ...baseConfig,
   accessToken: process.env.NEXT_PUBLIC_BNF_ROLLBAR_CLIENT_TOKEN ?? "",
+  payload: {
+    client: {
+      javascript: {
+        source_map_enabled: true,
+        code_version: process.env.NEXT_PUBLIC_BNF_VERSION ?? "",
+        guess_uncaught_frames: true,
+      },
+    },
+  },
 };
